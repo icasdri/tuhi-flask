45cdf81052549332f4dfc80c8e363a942785d0f2
icasdri <icasdri@gmail.com>
2015-07-01 21:52:50 -0400

    Fixed problem with sqlalchemy having trouble finding test db.

--- tuhi_flask/database.py
+++ tuhi_flask/database.py
@@ -19,7 +19,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///test.db', convert_unicode=True)
+engine = create_engine('sqlite:///../test.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
